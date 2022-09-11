2431ad0995d7cee825c7c3f46f2a10f8ce102261
Aminaev Roman <romanzovut@gmail.com>
2022-09-11 10:51:31 +0300

    Новый функционал. Домашнее задание №7 от 06.09.2022

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Motorcycle honda  = new Motorcycle(2,250);
+        Car peugeot206 = new Car (4,5);
+        honda.printInfo();
+        peugeot206.printInfo();
     }
 }
---
+++ src/Vehicle.java
@@ -0,0 +1,8 @@
+public abstract class Vehicle {
+    protected int wheelCount;
+    public Vehicle (int wheelCount) {
+        this.wheelCount = wheelCount;
+    }
+    protected abstract void  printInfo();
+
+}
